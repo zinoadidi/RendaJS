73a753eb4dda6e6e146abd9670e2d05c88f6a043
Zino Adidi <you@example.com>
2017-11-07 13:30:47 +0100

    Initial commit, typescipt + js implimentation

---
+++ src/migration.js
@@ -0,0 +1,212 @@
+ 
+	function Render(param){
+	    this.page = function(page) {
+	    	render.loader('start');
+	    	var url = this.config.viewPath+page;
+	    	$.get(url,{},function(data,status){
+	    		if(data) {
+		            $('#'+render.config.displayContainer).html(data);
+		            updateUrl(page,null);
+		            render.loader('stop');
+		            return 0;
+		        }else{
+		        	render.page('404');
+		   			render.log('page not found');
+		   			return 1;
+		        }
+		    }).fail(function() {
+		        render.page('404');
+		   		render.log('an error occured while loading page');
+		   		return 1;
+		    });
+	    };
+	    this.component = function(page,component,element) {
+	    	render.loader('start');
+	    	var url = this.config.viewPath+page+'Components'+'/'+component;
+	    	$.get(url,{},function(data,status){
+	    		if(data) {
+	    			$('#'+element).html(data);
+		            updateUrl(page,component);
+		            render.loader('stop');
+		            return 0;
+		        }else{
+		        	$('#'+element).html('component not found');
+		   			render.log('component not found');
+		   			return 1;
+		        }
+		    }).fail(function() {
+		        render.page('404');
+		   		render.log('an error occured while loading component');
+		   		return 1;
+		    });
+	    };
+	    function updateUrl(page,component) {
+	    	if (component) {
+	        	render.config.currentPage = page;
+	        	render.config.currentComponent = component;
+	            var title = render.config.appTitle+" | "+page+" - "+component;
+	            var stateObj = { page: page };
+	            document.title = title;
+				history.pushState(stateObj, title, "#!/"+page+'/'+component);
+	            render.log(page+" loaded");
+	    	}else{
+	    		render.config.currentPage = page;
+	            var stateObj = { page: page };
+	            var title = render.config.appTitle+" | "+page;
+	            document.title = title;
+				history.pushState(stateObj, title, "#!/"+page);
+	            render.log(page+" loaded");
+	    	}
+	    };
+	    this.start = function(){
+	    	if(typeof jQuery == 'undefined'){
+	    		if(this.config.appMode == 'debug'){
+	    			this.log('Render.JS - Warning: JQuery not Detected, Please make sure you have added JQuery library to your page before including Render.js');
+	    		}else{
+	    		}
+	    		document.write('...one or more required app components missing. Please contact webmaster for more information<br/>');
+	    	}else{
+	    		$( document ).ready(function() {
+	    			render.log('App Started');
+	   				/*Initiate Loading Indicator*/	   
+			    	if (render.config.loader.showTxt == true) {
+			    		var loader = 
+			    		"<div style ='"+render.config.loader.style+"' class='loader'>"+
+			    		render.config.loader.text+"</div>";	
+			    	}else{
+			    		var loader = 
+			    		"<img src='"+render.config.loader.imgUrl+"' style ='"+render.config.loader.style+"' class='loader'/>";
+			    	}
+			    	$('body').append(loader);
+			    	render.loader();
+	    			render.trackPageChange(true);
+	    			$(window).on('hashchange', function(e){
+	    				render.trackPageChange(true);
+					});
+				});    		
+	    	}  	
+	    };
+	    this.postData = function(url,data,method,header) {
+	    	render.loader('start');
+	    	if (method) {}else{render.log('fatal error, Please pass "method" param!');return false;}
+	        $.ajax({
+		        url: this.config.serverUrl+url,
+		        type: 'POST',
+		        data:data,
+		        beforeSend:function(xhr){
+		        	if(header){
+		            	xhr.setRequestHeader(header);	
+		        	}
+		        },
+		        success: function (result) {
+		            window[method](result,method);
+		        },
+		        error: function (result) {
+		            window[method](result,method);
+		        }
+		    });   
+	    };	    
+	    this.getData = function(url,method,callbackData,header) {
+	        render.loader('start');
+	        if (method) {}else{render.log('fatal error, Please pass "method" param!');return false;}
+	        if( callbackData){}else{callbackData = null};
+	        $.ajax({
+		        url: this.config.serverUrl+url,
+		        type: 'GET',
+		        data:data,
+		        beforeSend:function(xhr){
+		        	if(header){
+		            	xhr.setRequestHeader(header);	
+		        	}
+		        },
+		        success: function (result) {
+		            window[method](result,method, callbackData);
+		        },
+		        error: function (result) {
+		            window[method](result,method, callbackData);
+		        }
+		    });   
+	    };	    
+	    this.sendPut = function(url,data,method,header) {
+	        render.loader('start');
+	        if (method) {}else{render.log('fatal error, Please pass "method" param!');return false;}
+	        $.ajax({
+		        url: this.config.serverUrl+url,
+		        type: 'PUT',
+		        data:data,
+		        beforeSend:function(xhr){
+		        	if(header){
+		            	xhr.setRequestHeader(header);	
+		        	}
+		        },
+		        success: function (result) {
+		            window[method](result,method);
+		        },
+		        error: function (result) {
+		            window[method](result,method);
+		        }
+		    });   
+	    };
+	    this.loader = function(val) {
+	    	if (val == 'start') {
+	        	$('.loader').show();
+	        }else{
+	        	$('.loader').hide();
+	        }
+	    };  
+	    this.log = function(msg){
+	    	if (this.config.appMode =='debug') {
+	    		console.log(msg);
+	    	}else{}
+	    };
+	   this.trackPageChange = function(val){
+	   		if (val) {
+	   			var cPage = '';
+	   			cPage = window.location.href;
+	   			cPage = cPage.split('#!');
+	   			
+   				if (cPage[0]){
+   					if(cPage[1]){
+		   				cPage = cPage[1].split("/");
+		   				if (cPage[1] == '' || cPage[1] == null || cPage[1] == '/') {
+		   					this.page(this.config.defaultPage);
+		   				}else{
+		   					this.page(cPage[1]);
+		   					if(cPage[2]){
+			   					if (cPage[2] == '' || cPage[2] == null || cPage[1] == '/') {
+				   					render.log('not a components');
+				   				}else{
+				   					render.log('component',cPage[2]);
+				   				}				   				
+				   			}
+		   				}
+			   		}else{
+			   			render.log('page not detected');
+		   				this.page(this.config.defaultPage);
+		   			}
+   				}
+	   		}
+	   }
+	   this.generateTable = function (data,columns,table){
+	   		if (typeof dataTable != 'undefined') {
+	   			if ($.fn.dataTable.isDataTable( '#'+table ) ) {
+		            $('#vendortable').DataTable({
+		                destroy:true,
+		                data: data,
+		                columns: columns
+		            });
+			    }else{
+		            $('#'+table).DataTable( {
+		                destroy:true,
+		                data: data,
+		                columns:columns
+		            });
+		    	}
+		    	this.styleTable();
+	   		}else{
+	   			this.log('An error occured generating dataTable. Please add dataTable library before using this function');
+	   		}
+	        
+    	}
+	}
+	var render = new Render(null);
